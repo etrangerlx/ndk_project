a7d740d388b416d4f2e48892d6ff9e894e8601f0
etrangerlx <2285993564@qq.com>
2022-09-20 02:00:29 +0800

    修改内存跟踪类

--- NDK_02/include/MemoryTrace.h
+++ NDK_02/include/MemoryTrace.h
@@ -2,8 +2,8 @@
 // Created by rainbow on 2022/9/17.
 //
 
-#ifndef NEW_DELETE_MEMORYTRACE_H
-#define NEW_DELETE_MEMORYTRACE_H
+#ifndef NEW_DELETE_MEMORYTRACE_HPP
+#define NEW_DELETE_MEMORYTRACE_HPP
 
 #include <iostream>
 #include <vector>
@@ -13,27 +13,28 @@
 #include <typeinfo>
 
 using namespace std;
+bool Isdebug = false;
 
-class MemInfo {
-public:
+struct MemInfo {
     void *pMemAddress = nullptr;
-    string typeName ;
+    string typeName = "";
     size_t hashCode = 0;
-    string fileName ;
-    string funName ;
+    string fileName = "";
+    string funName = "";
     int Line = 0;
     bool isArray = false;
     int bMemsize = 0;
 
-    bool operator<(const MemInfo &t1) {
-        if (t1.hashCode <= hashCode) {
+    bool operator<(const MemInfo &m1) {
+        if (Line > m1.Line) {
             return true;
         } else {
             return false;
         }
     }
-    friend bool operator<(const MemInfo &t1,const MemInfo &t2){
-        if (t1.hashCode <= t2.hashCode) {
+
+    friend bool operator<(const MemInfo &t1, const MemInfo &t2) {
+        if (t1.Line <= t2.Line) {
             return true;
         } else {
             return false;
@@ -44,94 +45,119 @@ public:
 class MemoryTrace {
 public:
     MemoryTrace() {
-        MemToatalCur = 0;
-        MemToatalMax = 0;
+        MemTotalCur = 0;
+        MemTotalMax = 0;
 
         MembBlockCountCur = 0;
         MembBlockCountMax = 0;
     }
 
-    ~MemoryTrace(){
+    ~MemoryTrace() {
         if (MemMap.size() != 0) {
             printMemInfo();
             abort();
         }
     }
 
-    template<typename T>
-    T * NEW_RECORD(string classtype, size_t hash, \
-     string fN, bool isA, int Memsize, string funN,int line){
 
-        T * pMemBase = new T();
+    void NEW_RECORD(void *pMemBase, string classtype, size_t hash, \
+     string fN, bool isA, int Memsize, string funN, int line) {
+//        if (Isdebug) {
         MemInfo meminfotmp;
-        meminfotmp.pMemAddress= (void *)pMemBase;
+        meminfotmp.pMemAddress = (void *) pMemBase;
         meminfotmp.typeName = classtype;
         meminfotmp.hashCode = hash;
         meminfotmp.funName = funN;
         meminfotmp.isArray = isA;
         meminfotmp.bMemsize = Memsize;
         meminfotmp.fileName = fN;
-        meminfotmp.Line=line;
+        meminfotmp.Line = line;
 
-        MemToatalCur += Memsize;
+        MemTotalCur += Memsize;
         MembBlockCountCur++;
         //记录内存申请情况
-        if (MemToatalCur > MembBlockCountMax) {
-            MembBlockCountMax = MemToatalCur;
+        if (MemTotalCur > MemTotalMax) {
+            MemTotalMax = MemTotalCur;
         }
-        if (MembBlockCountCur > MemToatalMax) {
-            MemToatalMax = MembBlockCountCur;
+        if (MembBlockCountCur > MembBlockCountMax) {
+            MembBlockCountMax = MembBlockCountCur;
         }
-        clock_t t =clock();
+        clock_t t = clock();
 
-        MemMap.insert(pair<MemInfo,size_t>(meminfotmp,t));
-        return pMemBase;
+        MemMap.insert(pair<MemInfo, clock_t>(meminfotmp, t));
+        cout << "new MEMSIZE:" << MemMap.size() << endl;
+//        } else {
+//
+//        }
+
+
     }
 
-    template <typename T>
-    void DELETE_RECORD(T *pMemBase, bool isA){
-        for (map<MemInfo,clock_t>::iterator itr = MemMap.begin(); itr != MemMap.end(); itr++) {
-            if ((T*)itr->first.pMemAddress == pMemBase) {
-                MemToatalCur -= itr->first.bMemsize;
-                MembBlockCountCur--;
+    void DELETE_RECORD(void *pMemBase, bool isA) {
+        for (auto itr = MemMap.begin(); itr != MemMap.end(); itr++) {
+//            cout << "pMemBase:"<< pMemBase<< endl;
+//            cout << "itr:"<<itr->first.pMemAddress << endl;
+            if (itr->first.pMemAddress == pMemBase) {
+                MemTotalCur -= itr->first.bMemsize;
+                MembBlockCountCur --;
+                delete pMemBase;
                 MemMap.erase(itr);
-                cout<<pMemBase<<endl;
-                delete(pMemBase);
+//                delete &itr;
+//                cout << pMemBase << endl;
+
+//                cout << pMemBase << endl;
                 pMemBase = nullptr;
+                break;
             }
         }
+        cout << "remain MEMSIZE:" << MemMap.size() << endl;
     }
 
-    bool isNormall(){
+    bool isNormall() {
         //TODO
     }
 
-    void printMemInfo(){
-        for(map<MemInfo,clock_t>::iterator itr= MemMap.begin();itr != MemMap.end();itr++){
-            cout<<"FILENAME:"<<itr->first.fileName<<","
-            <<"FUNCTION:"<<itr->first.funName<<","
-            <<"["<<itr->first.Line<<"]"<<endl;
+    void printMemInfo() {
+        for (auto itr = MemMap.begin(); itr != MemMap.end(); itr++) {
+            cout << "FILENAME:" << itr->first.fileName << ","
+                 << "FUNCTION:" << itr->first.funName << ","
+                 << "[" << itr->first.Line << "]" << endl;
         }
+        cout << "Current:" << MembBlockCountCur << endl;
+        cout << "Maxinum:" << MembBlockCountMax << endl;
+        cout << "Current:" << MemTotalCur << endl;
+        cout << "Maxinum:" << MemTotalMax << endl;
     }
 
 private:
     //记录内存总量
-    long long MemToatalCur;
-    long long MemToatalMax;
+    long long MemTotalCur;
+    long long MemTotalMax;
     //记录内存块总量
     int MembBlockCountCur;
     int MembBlockCountMax;
     //内存信息|时间信息
     map<MemInfo, clock_t> MemMap;
+//    vector<MemInfo> MemVec;
 };
+//long long MemoryTrace::MemTotalCur = 0;
+//long long MemoryTrace::MemTotalMax = 0;
+//int MemoryTrace::MembBlockCountCur = 0;
+//int MemoryTrace::MembBlockCountMax = 0;
+//map<MemInfo, clock_t> MemoryTrace::MemMap;
+static MemoryTrace MEM;
+#define ALGO_NEW(T,pVoid) {\
+    pVoid =new T;\
+    cout<<"new:"<<pVoid<<endl;\
+    MEM.NEW_RECORD(pVoid,typeid(T).name(),typeid(T).hash_code(),string(__FILE__),\
+                        false, sizeof(T),string(__FUNCTION__),int(__LINE__));\
+}
 
-static MemoryTrace MEM;//静态全局变量，用于存储、释放内存new、delete堆栈信息
-
-#define ALGO_NEW(T) MEM.NEW_RECORD<T>(typeid(T).name(),typeid(T).hash_code(),string(__FILE__),\
-                        false, sizeof(T),string(__FUNCTION__),int(__LINE__))
-
-#define ALGO_DELETE(T,pVoid) MEM.DELETE_RECORD<T>(pVoid,false)
-#endif //NEW_DELETE_MEMORYTRACE_H
+#define ALGO_DELETE(pVoid) {\
+cout<<"delete:"<<pVoid<<endl;\
+MEM.DELETE_RECORD(pVoid,false);\
+}
+#endif //NEW_DELETE_MEMORYTRACE_HPP
 
 
 
--- NDK_02/test.cpp
+++ NDK_02/test.cpp
@@ -1,20 +1,80 @@
-//
-// Created by rainbow on 2022/9/17.
-//
-#include "include/MemoryTrace.h"
 
-//void *operator new(size_t size){
-//    cout<<"new"<<endl;
-//    return malloc(size);
+#include "MemoryTrace.hpp"
+#include <unistd.h>
+//变量类型
+//https://blog.csdn.net/weixin_45743799/article/details/104861109
+
+//void *operator new(size_t size) {
+//    void *p = nullptr;
+//    p = malloc(size);
+//
+//    if (p == nullptr) {
+//        cout << "new error" << endl;
+//        return nullptr;
+//    } else {
+//
+//        cout << "new object addr:" << p << "," << "size:" << size << endl;
+//    }
+//    return p;
+//}
+//void operator delete(void *pVoid) {
+//    if (pVoid != nullptr) {
+//        free(pVoid);
+//        cout << "delete:" << pVoid << endl;
+//        pVoid = nullptr;
+//    }
+//}
+//void operator delete[] (void *pVoid){
+//    if(pVoid != nullptr){
+//        free(pVoid);
+//        cout<<"delete[]:"<<pVoid<<endl;
+//        pVoid = nullptr;
+//    }
 //}
 
 class object {
 public:
     int x = 0;
     int y = 0;
+
+    void print() {
+        cout << x << endl;
+        cout << y << endl;
+    }
+
+    object() {
+        cout << "construct object:" << this << endl;
+    }
+
+    ~object() {
+        cout << "deconstruct object:" << this << endl;
+    }
 };
-int main(){
-    object *obj = ALGO_NEW(object);
-    ALGO_DELETE(object,obj);
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    string str = "xian.lei";
+    object *obj = nullptr;
+    object *obj1 = nullptr;
+    object *obj2 = nullptr;
+    ALGO_NEW(object,obj);
+
+    ALGO_NEW(object,obj1);
+    ALGO_NEW(object,obj2);
+
+    int *p1 = nullptr;
+    ALGO_NEW(int,p1);
+    ALGO_DELETE(p1);
+//    object *obj = MEM.NEW_RECORD<object>(typeid(object).name(),typeid(object).hash_code(),
+//            string(__FILE__),false, sizeof(object),string(__FUNCTION__),int(__LINE__));
+//    sleep(1);
+    ALGO_DELETE(obj1);
+//    delete obj1;
+//    sleep(1);
+//    MemoryTrace::printMemInfo();
+    ALGO_DELETE(obj2);
+    ALGO_DELETE(obj);
+//    delete obj2;
+//    MEM.DELETE_RECORD<object>(obj,false);
     return 0;
 }
